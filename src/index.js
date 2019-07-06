f8e9a39ce3cb118b899ebcf25ff6ca7e6fc93cd1
jim5 <abdumalikk94@gmail.com>
2019-07-06 22:50:55 +0500

    first c

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+ module.exports = function getLoveTriangleschislo(preferences = []) {
+ 	var dlina = preferences.length; 
+ 	var chislo = 0;  
+ 	for (var i = 0; i < dlina; i++) 
+ 	{ 
+ 		var t = preferences[i] - 1;
+ 		if (t == i) continue;
+ 		var t2 = preferences[t] - 1;
+ 		if (t == t2) continue;
+ 		var t3 = preferences[t2] - 1;
+ 		if (t3 == t2) continue;
+ 		if (t3 == i) 
+ 		{
+ 			chislo++;
+ 		}
+ 	}
+ 	return chislo / 3;
+ };
+
